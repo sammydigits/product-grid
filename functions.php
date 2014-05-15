20b6a47616f10ee2c04decc30c325497c994279c
Sam Thompson <sam.thompson@gmail.com>
2014-05-15 12:54:17 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function theme_styles() {
   
   wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . 'bootstrap.min.css' );
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . 'style.css' );
   
 }
 
