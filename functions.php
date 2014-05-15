d03ac38921f7619f31af26985e7b3729c99e033e
Sam Thompson <sam.thompson@gmail.com>
2014-05-15 13:04:17 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,4 +9,18 @@ function theme_styles() {
 
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
+function theme_js() {
+  
+  global $wp_scripts;  
+
+  wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js', '', '', false );
+  wp_register_script( 'respond_js', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', '', '', false );
+
+  $wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
+  $wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
+  
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_js' );
+
 ?>
