964bb3ff001e06449e4c887df83c03eb9b5a5e19
Sam Thompson <sam.thompson@gmail.com>
2014-05-15 13:05:09 -0500

    Update header.php

--- header.php
+++ header.php
@@ -11,12 +11,6 @@
 
     <title>Jumbotron Template for Bootstrap</title>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
     <?php wp_head(); ?>
     
   </head>
