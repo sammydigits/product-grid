42054bb66d28abf6a02c448f5bcefaa3c8241d70
Sam Thompson <sam.thompson@gmail.com>
2014-05-15 12:56:52 -0500

    Update header.php

--- header.php
+++ header.php
@@ -11,9 +11,6 @@
 
     <title>Jumbotron Template for Bootstrap</title>
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
