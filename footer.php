243d799cbd6969e81764f6588c00a5aa06f3b55c
Sam Thompson <sam.thompson@gmail.com>
2014-05-15 13:18:06 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,10 +6,6 @@
     </div> <!-- /container -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
   </body>
 </html>
