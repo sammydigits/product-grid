78b0704fd678f30aaf0f4538aaaf7a66c272d9e9
Sam Thompson <sam.thompson@gmail.com>
2014-05-19 13:17:24 -0500

    Update header.php

--- header.php
+++ header.php
@@ -26,18 +26,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
         </div>
+        
         <div class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
+
+          <?php
+            $args = array(
+              'menu'        => 'header-menu',
+              'menu_class'  => 'nav navbar-nav',
+              'container'   => 'false'
+            );
+            wp_nav_menu( $args );
+          ?>
+
         </div><!--/.navbar-collapse -->
       </div>
     </div>
