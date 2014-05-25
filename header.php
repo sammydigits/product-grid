6d6c9165e4f9c6e88ad4e565d62ebd3b44b1eab4
Sam Thompson <sam.thompson@gmail.com>
2014-05-25 14:50:38 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,13 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
-
-    <title>Jumbotron Template for Bootstrap</title>
+    <title>
+      <?php wp_title( '|', true, 'right' ); ?>
+      <?php bloginfo( 'name' ); ?>
+    </title>
 
     <?php wp_head(); ?>
     
