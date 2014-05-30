42432f65b88fa3177a19c0b78aed0a025f4a9c5f
Sam Thompson <sam.thompson@gmail.com>
2014-05-29 20:21:14 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -36,4 +36,25 @@ function register_theme_menus() {
   
 }
 add_action( 'init', 'register_theme_menus' );
+
+
+function create_widget( $name, $id, $description ) {
+
+	register_sidebar(array(
+		'name' => __( $name ),	 
+		'id' => $id, 
+		'description' => __( $description ),
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>'
+	));
+
+}
+
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the homepage' );
+create_widget( 'Front Page Center', 'front-center', 'Displays in the center of the homepage' );
+create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the homepage' );
+
+
 ?>
