807a7d7e98740f40a9c06f322d799ac04a788154
Sam Thompson <sam.thompson@gmail.com>
2014-06-01 15:12:45 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -5,18 +5,24 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <div class="col-md-4">
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <p>
-        By <?php the_author(); ?> 
-        on <?php echo the_time('l, F jS, Y');?>
-        in <?php the_category( ', ' ); ?>.
-      </p>
+      <div class="col-md-4">
 
-      <hr>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p>
+          By <?php the_author(); ?> 
+          on <?php echo the_time('l, F jS, Y');?>
+          in <?php the_category( ', ' ); ?>.
+        </p>
+  
+        <hr>
+  
+        <?php the_excerpt(); ?>
+      </div>
 
-      <?php the_excerpt(); ?>
-    </div>
+      <?php $post_count = $the_query->current_post + 1; ?>
+      <?php if ( $post_count % 4 == 0): ?>
+
+      </div><div class="row">
 
 <?php endwhile; else: ?>
   
