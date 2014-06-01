2f1b0992c661517257f47e5ccaac99e95a4d6a1c
Sam Thompson <sam.thompson@gmail.com>
2014-06-01 15:06:59 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,48 +1,34 @@
 <?php get_header(); ?>
 
-  <div class="container">   
-    <div class="row">
-      
-      <div class="col-md-9">
+<div class="container">
+  <div class="row">
 
-        <div class="page-header">
-          <h1><?php wp_title(''); ?></h1>
-        </div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="col-md-4">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <p>
+        By <?php the_author(); ?> 
+        on <?php echo the_time('l, F jS, Y');?>
+        in <?php the_category( ', ' ); ?>.
+      </p>
 
-          <article class="post">
-            
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <p>
-              By <?php the_author(); ?> 
-              on <?php echo the_time('l, F jS, Y');?>
-              in <?php the_category( ', ' ); ?>.
-              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
-            </p>
+      <hr>
 
-            <hr>
-
-            <?php the_excerpt(); ?>
-
-          </article>
-
-         
-        <?php endwhile; else: ?>
-          
-          <div class="page-header">
-            <h1>Oh no!</h1>
-          </div>
-
-          <p>No content is appearing for this page!</p>
-
-        <?php endif; ?>
-
-
-      </div>
-      
-      <?php get_sidebar( 'blog' ); ?>
-
+      <?php the_excerpt(); ?>
     </div>
+
+<?php endwhile; else: ?>
+  
+  <div class="page-header">
+    <h1>Nothing to see here.</h1>
+  </div>
+
+  <p>No posts found.</p>
+
+<?php endif; ?>       
+
+  </div>
+</div>
 
 <?php get_footer(); ?>
