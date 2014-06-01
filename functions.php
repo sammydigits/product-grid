5be552578ad45248075384198c4f19d44a663b57
Sam Thompson <sam.thompson@gmail.com>
2014-06-01 14:35:25 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,39 +1,39 @@
 <?php
 
-function theme_styles() {
-  
-  wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/bootstrap.min.css' );
-  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-  
-}
 
+function theme_styles() {
+
+	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
 function theme_js() {
-  
-  global $wp_scripts;  
 
-  wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js', '', '', false );
-  wp_register_script( 'respond_js', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', '', '', false );
+	global $wp_scripts;
 
-  $wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
-  $wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
-  
-  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/bootstrap.min.js', array('jquery') );
+	wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js', '', '', false );
+	wp_register_script( 'respond_js', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', '', '', false );
+
+	$wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
+	$wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
+
+	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
+
 }
-
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
-add_theme_support('menus');
+//add_filter( 'show_admin_bar', '__return_false' );
+
+add_theme_support( 'menus' );
 
 function register_theme_menus() {
-  
-  register_nav_menus(
-    array(
-      'header-menu' => __('Header Menu')
-    )  
-  );
-  
+	register_nav_menus(
+		array(
+			'header-menu'	=> __( 'Header Menu' )
+		)
+	);
 }
 add_action( 'init', 'register_theme_menus' );
 
@@ -55,6 +55,15 @@ function create_widget( $name, $id, $description ) {
 create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the homepage' );
 create_widget( 'Front Page Center', 'front-center', 'Displays in the center of the homepage' );
 create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the homepage' );
+
+
+create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
+create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
+
+
+
+
+
 
 
 ?>
