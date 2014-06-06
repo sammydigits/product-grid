72f2d363e8c2235c4f327ec0e1b2f72bbf615065
Sam Thompson <sam.thompson@gmail.com>
2014-06-05 22:48:36 -0500

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -10,6 +10,12 @@
   if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
       <div class="col-md-4">
+      
+      	<?php if ( has_post_thumbnail()) : ?>
+          <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+          <?php the_post_thumbnail('medium'); ?>
+        </a>
+        <?php endif; ?>
 
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <p>
