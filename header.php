71aff4ef3f650d9cebadbe76c93e5f06777b399f
Sam Thompson <sam.thompson@gmail.com>
2014-06-05 22:41:29 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
   <body <?php body_class(); ?>>
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
