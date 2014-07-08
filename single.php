4b2bcb4889440c76dc17ce2e3bc24a27d43510df
Sam Thompson <sam.thompson@gmail.com>
2014-07-08 13:30:04 -0500

    Update single.php

--- single.php
+++ single.php
@@ -9,10 +9,7 @@
 
           <div class="page-header">
             <h1><?php the_title(); ?></h1>
-            <p>
-              Selected on <?php echo the_time('l, F jS, Y');?>
-              in <?php the_category( ', ' ); ?>.
-            </p>
+            <p>Selected on <?php echo the_time('l, F jS, Y');?>, stored in the <?php the_category( ', ' ); ?> department.</p>
           </div>
 
           <div class="entry-content">
