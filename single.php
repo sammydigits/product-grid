e04c6a5bc6782e85eb8f9c434c14d42ce113fca4
Sam Thompson <sam.thompson@gmail.com>
2014-07-10 13:11:44 -0500

    Update single.php

--- single.php
+++ single.php
@@ -6,15 +6,19 @@
       <div class="col-md-9">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        
+        <div class="row">
 
-          <div class="page-header">
+          <div class="page-header col-md-4">
             <h1><?php the_title(); ?></h1>
             <p>Selected on <?php echo the_time('l, F jS, Y');?>, stored in the <?php the_category( ', ' ); ?> department.</p>
           </div>
 
-          <div class="entry-content">
+          <div class="entry-content col-md-8">
             <?php the_content(); ?>
           </div>
+
+        </div>
 
         <?php endwhile; else: ?>
           
